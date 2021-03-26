2f1c70dd7bbe9c3a6a348b5e29d972ac1b63b862
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2021-03-26 16:05:40 +0100

    Implement the timer example

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use bevy::prelude::*;
 
+struct GreetTimer(Timer);
+
 pub struct HelloPlugin;
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut AppBuilder) {
         // add thing to app
-        app.add_startup_system(add_people.system())
-            .add_system(hello_world.system())
+        app.add_resource(GreetTimer(Timer::from_seconds(2.0, true)))
+            .add_startup_system(add_people.system())
             .add_system(greet_people.system());
     }
 }
@@ -33,16 +35,16 @@ fn print_position_system(query: Query<&Transform>) {
         println!("position: {:?}", transform.translation);
     }
 }
-fn hello_world() {
-    println!("hello, world!");
-}
 fn add_people(commands: &mut Commands) {
     commands
         .spawn((Person, Name("Bob Bobson".to_string())))
         .spawn((Person, Name("Flip Flipper".to_string())))
         .spawn((Person, Name("Henny Henderson".to_string())));
 }
-fn greet_people(query: Query<&Name, With<Person>>) {
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if !timer.0.tick(time.delta_seconds()).just_finished() {
+        return;
+    }
     for Name(name) in query.iter() {
         println!("hello {}!", name);
     }
